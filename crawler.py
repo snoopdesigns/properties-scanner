a9089ec98ac2e11ea0b3f8affa348fa68ce5152b
snoopdesigns <snoopdesigns@gmail.com>
2017-11-14 21:37:54 +0300

    test

--- crawler.py
+++ crawler.py
@@ -7,6 +7,9 @@ from pyquery import PyQuery as pq
 from lxml import etree
 import time
 import json
+import xml.etree.ElementTree as ET
+from math import radians, cos, sin, asin, sqrt
+import os
 import myconfig
 
 TELEGRAM_TOKEN = "453478799:AAGyWsQm2B28Yok4I81EGkv-CHA58lPlgDo"
@@ -14,6 +17,11 @@ TELEGRAM_CHAT_ID = "116360945"
 AVITO_DATA_FILE_PATH = myconfig.avito_datafile
 AVITO_URL = myconfig.avito_url
 POINTS_DATA_FILE = myconfig.points_datafile
+INFORM = myconfig.inform_flag
+DIST_THRESHOLD = myconfig.dist_threshold
+
+points = []
+names = []
 
 new_apartments_avito = []
 
@@ -64,7 +72,12 @@ def process_page_avito(html, write_header, existing_ids_avito, millis):
 		for idx, val in enumerate(infos):
 			if ids[idx] not in existing_ids_avito:
 				#print("Found unique id:" + str(ids[idx]))
-				new_apartments_avito.append("https://www.avito.ru%" + urls[idx])
+				if find_matches(address[idx]) == 1:
+					new_apartments_avito.append("MATCH:\nhttps://www.avito.ru%" + urls[idx])
+					print("https://www.avito.ru%s" % urls[idx])
+				else:
+					#print("Match not found: " + address[idx])
+					new_apartments_avito.append("https://www.avito.ru%" + urls[idx])
 				writer.writerow({'id': ids[idx], 'url': urls[idx], 'info': infos[idx], 'price': prices[idx], 'address': address[idx], 'timestamp': millis})
 			else:
 				print("Found already existing id:" + str(ids[idx]))
@@ -88,6 +101,8 @@ def run_avito_crawler():
 		url = AVITO_URL + "&p=" + str(page)
 		html = load_page(url)
 		write_header = 1
+		if os.path.getsize(AVITO_DATA_FILE_PATH)==0:
+			write_header = 0
 		if page > 1:
 			write_header = 0
 		page_len = process_page_avito(html, write_header, existing_ids_avito, millis)
@@ -96,25 +111,51 @@ def run_avito_crawler():
 		if (page_len < 50):
 			break
 
-def geocode_addr(addr):
-	addr = urllib.parse.quote_plus(addr)
-	url = "https://geocode-maps.yandex.ru/1.x/?format=json&results=1&geocode=" + addr
-	fp = urllib.request.urlopen(url)
-	mybytes = fp.read()
-	mystr = mybytes.decode("utf8")
-	fp.close()
-	json_res = json.loads(mystr)
-	print(json_res["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"])
-	return mystr
+def dist(left, right):
+	lon1 = float(left.split(",")[0])
+	lat1 = float(left.split(",")[1])
+	lon2 = float(right.split(",")[0])
+	lat2 = float(right.split(",")[1])
+	lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+	dlon = lon2 - lon1 
+	dlat = lat2 - lat1 
+	a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+	c = 2 * asin(sqrt(a))
+	km = 6371* c
+	return km
 
-points  = open(POINTS_DATA_FILE, 'r').read()
-d = pq(points)
-print(d)
-#print(d("Point"))
-for item in d("Placemark"):
-	print(item.text)
-#result = geocode_addr("Санкт-Петербург, м. Улица Дыбенко, Искровский пр-кт, д.28")
-#run_avito_crawler()
-#inform_load()
+def find_matches(addr):
+	addr = urllib.parse.quote_plus(addr)
+	with urllib.request.urlopen('https://geocode-maps.yandex.ru/1.x/?results=1&geocode=' + addr) as response:
+	   html = response.read()
+	tree = ET.fromstring(html)
+	coord = "undefined"
+	for point in tree.iter('{http://www.opengis.net/gml}Point'):
+		coord = point[0].text.strip().split(" ")[0] + "," + point[0].text.strip().split(" ")[1]
+	#print("Target coord: " + coord)
+
+	if coord=="undefined":
+		return 0
+	for idx, point in enumerate(points):
+		distance = dist(point, coord)
+		if (distance < DIST_THRESHOLD):
+			print("Match found: %s" % names[idx])
+			#print(point)
+			#print(names[idx])
+			return 1
+	return 0
+
+def prepare_points():
+	tree = ET.parse(POINTS_DATA_FILE)
+	root = tree.getroot()
+	for point in root.iter('{http://www.opengis.net/kml/2.2}Placemark'):
+		points.append(point[2][0].text.strip().split(",")[0] + "," + point[2][0].text.strip().split(",")[1])
+		names.append(point[0].text.strip())
+	print("Loaded %d points from file" % len(points))
+
+prepare_points()
+run_avito_crawler()
+if INFORM=="1":
+	inform_load()
 
 
--- myconfig.py
+++ myconfig.py
@@ -1,3 +1,5 @@
 points_datafile="data/points.xml"
 avito_datafile="data/avito-data.csv"
 avito_url="https://www.avito.ru/sankt-peterburg/kvartiry/sdam/na_dlitelnyy_srok/1-komnatnye?s=101&user=1&metro=196-208"
+inform_flag="0"
+dist_threshold=0.05
